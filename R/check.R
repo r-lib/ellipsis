f01ecfbcd9a2e1ab4773cc180e5a000ea30bc5b8
hadley <h.wickham@gmail.com>
2018-07-06 16:03:38 -0500

    Export and document check_dots_used

--- R/check.R
+++ R/check.R
@@ -1,3 +1,25 @@
+#' Check that all dots in current environment have been used
+#'
+#' Typically used in `on.exit()`. Generates warning if any dots have not
+#' been forced (i.e. evaluated)
+#'
+#' @param env Environment in which to look for `...`. This is a temporary
+#'   API until we have a C API that can follow promises up multiple levels
+#'   in the call stack.
+#' @export
+#' @examples
+#' f <- function(...) {
+#'   on.exit(check_dots_used())
+#'   g(...)
+#' }
+#'
+#' g <- function(x, y, ...) {
+#'   x + y
+#' }
+#' f(x = 1, y = 2)
+#'
+#' f(x = 1, y = 2, z = 3)
+#' f(x = 1, y = 2, 3, 4, 5)
 check_dots_used <- function(env = caller_env()) {
   proms <- env_dots_promises(env)
   used <- vapply(proms, promise_forced, logical(1))
