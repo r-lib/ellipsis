c3a1e6feda4b95cabaa671409dea9f8d0c2309b4
Hadley Wickham <h.wickham@gmail.com>
2019-02-18 16:25:31 -0600

    Test for false positives

--- tests/testthat/test-safe.R
+++ tests/testthat/test-safe.R
@@ -2,5 +2,6 @@ context("test-safe")
 
 test_that("warn if unused dots", {
   expect_warning(safe_median(1:10), NA)
+  expect_warning(safe_median(1:10, na.rm = TRUE), NA)
   expect_warning(safe_median(1:10, y = 1), "were not used")
 })
