2a3e972555992b4ec2b35eb7ed9b0e8a5beac76f
Salim B <salim@posteo.de>
2020-06-14 23:07:43 +0200

    Overhaul `check_dots_named()`:
    - provide name of the function the dots are meant for (passed on to) in the error message
    
    - improve error message when no named dots are allowed.
    
    - explicitly state in error message when a `.forbidden` dot arg name is provided.
    
    - improve/extend documentation and examples

--- R/check.R
+++ R/check.R
@@ -132,16 +132,47 @@ action_dots <- function(action, message, dot_names, note = NULL, .subclass = NUL
 
 #' Check that all dot parameter names are a valid subset of a function's parameter names.
 #'
+#' This function ensures that [dots (...)][base::dots()] are either empty (if `.empty_ok = TRUE`), or all named dot parameter names are a valid subset of a
+#' function's parameter names. In case of an invalid or `.forbidden` argument, an informative message is shown and the defined `.action` is taken.
+#'
+#' `check_dots_named()` is intended to combat the second one of the two major downsides that using `...` usually brings. In chapter 6.6 of the book
+#' _Advanced R_ it is [phrased](https://adv-r.hadley.nz/functions.html#fun-dot-dot-dot) as follows:
+#'
+#' _Using `...` comes with two downsides:_
+#'
+#' - _When you use it to pass arguments to another function, you have to carefully explain to the user where those arguments go. This makes it hard to
+#'   understand what you can do with functions like `lapply()` and `plot()`._
+#'
+#' - **_A misspelled argument will not raise an error. This makes it easy for typos to go unnoticed._**
+#'
 #' @param ... The dots argument to check.
 #' @param .function The function the `...` will be passed on to.
 #' @param .forbidden Parameter names within `...` that should be treated as
-#'   invalid.
+#'   invalid. A character vector.
 #' @param .empty_ok Set to `TRUE` if empty `...` should be allowed, or to `FALSE`
 #'   otherwise.
 #' @param .action The action to take when the check fails. One of [rlang::abort()],
 #'   [rlang::warn()], [rlang::inform()] or [rlang::signal()].
 #' @export
 #' @examples
+#' # We can use `check_dots_named()` to address this second downside:
+#' sum_safe <- function(...,
+#'                      na.rm = FALSE) {
+#'   check_dots_named(...,
+#'                    .function = sum)
+#'   sum(...,
+#'       na.rm = na.rm)
+#' }
+#'
+#' # note how the misspelled `na_rm` (instead of `na.rm`) silently gets ignored
+#' # in the original function
+#' sum(1, 2, NA, na_rm = TRUE)
+#'
+#' \dontrun{
+#' # whereas our safe version properly errors
+#' sum_safe(1, 2, NA, na_rm = TRUE)}
+#'
+#' # we can even build an `sapply()` function that fails "intelligently"
 #' sapply_safe <- function(X,
 #'                         FUN,
 #'                         ...,
@@ -156,25 +187,64 @@ action_dots <- function(action, message, dot_names, note = NULL, .subclass = NUL
 #'          USE.NAMES = TRUE)
 #' }
 #'
-#' \dontrun{
 #' # while the original `sapply()` silently ignores misspelled arguments,
 #' sapply(1:5, paste, "hour workdays", sep = "-", colaspe = " ")
-#' # `sapply_safe()` will throw an informative error message:
-#' sapply_safe(1:5, paste, "hour workdays", sep = "-", colaspe = " ")
-#' }
+#'
+#' \dontrun{
+#' # `sapply_safe()` will throw an informative error message
+#' sapply_safe(1:5, paste, "hour workdays", sep = "-", colaspe = " ")}
+#'
+#' \dontrun{
+#' # but be aware that `check_dots_named()` might be a bit rash
+#' sum_safe(a = 1, b = 2)}
+#'
+#' # while the original function actually has nothing to complain about
+#' sum(a = 1, b = 2)
+#'
+#' \dontrun{
+#' # also, it doesn't play nicely with functions that don't expose all of
+#' # their arg names (`to` and `by` in the case of `seq()`)
+#' sapply_safe(X = c(0,50),
+#'             FUN = seq,
+#'             to = 100,
+#'             by = 5)}
+#'
+#' # but providing `to` and `by` *unnamed* is fine of course:
+#' sapply_safe(X = c(0,50),
+#'             FUN = seq,
+#'             100,
+#'             5)
 check_dots_named <- function(...,
                              .function,
                              .forbidden = NULL,
                              .empty_ok = TRUE,
                              .action = abort) {
   if (length(list(...))) {
+
+    # determine original function name the `...` will be passed on to
+    fun_arg_name <- deparse1(substitute(.function))
+    parent_call <- as.list(sys.call(-1L))
+    parent_param_names <- methods::formalArgs(sys.function(-1L))
+
+    if (fun_arg_name %in% parent_param_names) {
+      fun_name <- as.character(parent_call[which(parent_param_names == fun_arg_name) + 1][[1]])
+    } else {
+      fun_name <- fun_arg_name
+    }
+
+    # determine param names of the function the `...` will be passed on to
+    dots_param_names <- methods::formalArgs(checkmate::assert_function(.function))
+
+    # check named `...` args
     purrr::walk(
       .x = setdiff(names(c(...)), ""),
       .f = check_dot_named,
-      values = setdiff(methods::formalArgs(.function),
-                       checkmate::assert_character(.forbidden,
-                                                   any.missing = FALSE,
-                                                   null.ok = TRUE)),
+      values = dots_param_names,
+      allowed_values = setdiff(dots_param_names,
+                               checkmate::assert_character(.forbidden,
+                                                           any.missing = FALSE,
+                                                           null.ok = TRUE)),
+      fun_name = fun_name,
       action = .action
     )
 
@@ -184,25 +254,45 @@ check_dots_named <- function(...,
   }
 }
 
+# The following code is largely borrowed from `rlang::arg_match()`
 check_dot_named <- function(dot,
                             values,
+                            allowed_values,
+                            fun_name,
                             action) {
-  # The following code is largely borrowed from `rlang::arg_match()`
-  i <- match(dot, values)
+  i <- match(dot, allowed_values)
 
   if (is_na(i)) {
-    msg <- paste0("Invalid argument provided in `...`: `", dot,
-                  "`\nValid arguments include: ", prose_ls(values, wrap = "`"))
-    i_partial <- pmatch(dot, values)
 
-    if (!is_na(i_partial)) {
-      candidate <- values[[i_partial]]
+    is_forbidden <- dot %in% values
+    is_restricted <- !setequal(values,
+                               allowed_values)
+
+    msg <- paste0(ifelse(is_forbidden, "Forbidden", "Invalid"),
+                  " argument provided in `...`: `", dot, "`\n")
+
+    if (length(allowed_values) > 0) {
+      msg <- paste0(msg, ifelse(is_restricted,
+                                "Arguments allowed to pass on to ",
+                                "Valid arguments for "),
+                    "`", fun_name, "()` include: ",
+                    prose_ls(allowed_values, wrap = "`"), "\n")
+    } else {
+      msg <- paste0(msg, "Only unnamed arguments are ",
+                    ifelse(is_restricted, "allowed", "valid"),
+                    " for `", fun_name, "()`.")
     }
 
-    i_close <- utils::adist(dot, values)/nchar(values)
+    i_partial <- pmatch(dot, allowed_values)
+
+    if (!is_na(i_partial)) {
+      candidate <- allowed_values[[i_partial]]
+    }
+
+    i_close <- utils::adist(dot, allowed_values)/nchar(allowed_values)
 
     if (any(i_close <= 0.5)) {
-      candidate <- values[[which.min(i_close)]]
+      candidate <- allowed_values[[which.min(i_close)]]
     }
 
     if (exists("candidate")) {
